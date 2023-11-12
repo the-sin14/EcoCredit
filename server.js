7a027acc49ec05062f4b0367f11570a312eaaa95
dayee0318 <dy0318m@gmail.com>
2023-11-12 02:01:26 -0700

    server updated

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path'; // Import the 'path' module
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
-// Define a port
 const port = process.env.PORT || 3000;
 
-app.use(express.json()); // for parsing application/json
+app.use(express.json());
 
-// Define a route
+// Serve static files from the 'public' folder
+app.use(express.static(__dirname + '/public'));
+
+// Define a route to serve the CSV file
+app.get('/data/sample-spending.csv', (req, res) => {
+  const filePath = path.resolve(__dirname, 'src', 'scripts', 'sample-spending.csv');
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.error('Error sending file:', err);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+});
+
+// Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 // Start the server
